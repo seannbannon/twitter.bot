c9672cdc64009dcb7f87a344747cb87d5c04d739
Sean Bannon <seannbannon@gmail.com>
2021-07-23 12:49:18 -0500

    checking again

--- config.js
+++ config.js
@@ -1,11 +1,6 @@
-module.exports = {
-    consumer_key: 'zKjYHHJzkliQZYZYXO8q4xyjS',
-    consumer_secret: 'dp5DXVdHTzf6fDhtR2tnFT0FVUfylki2vpex8bHmoIo2VUBgnH',
-    access_token_key: 'AAAAAAAAAAAAAAAAAAAAAAveRwEAAAAA9IPpjEEECt0o%2Bxzw3%2BdkFBsotF4%3DqtE4vD5kfQJsR1YVKfaYqS5Xre3hQY6HiH5zLR867igGrh0n7E',
-    access_token_secret: ''
+module.exports = {    
+
   }
 
 
-//   bearer token AAAAAAAAAAAAAAAAAAAAAAveRwEAAAAA9IPpjEEECt0o%2Bxzw3%2BdkFBsotF4%3DqtE4vD5kfQJsR1YVKfaYqS5Xre3hQY6HiH5zLR867igGrh0n7E
-
-
+//   bearer token AAAAAAAAAAAAAAAAAAA
